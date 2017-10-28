b5bc6e5f1d2684383147bf30733b0d595120704d
Yu-Jing Lin <linx1045@umn.edu>
2017-10-28 18:24:36 +0800

    revised again

--- hw1/model_cnn.py
+++ hw1/model_cnn.py
@@ -1,6 +1,7 @@
+import sys
 from mypackages.utils import *
 from mypackages.models import train_rnn
 
-load_timit_dataset()
+load_timit_dataset(dataset_path=sys.argv[1])
 X_train, y_train, _, _, _ = preprocess()
 train_cnn(X_train, y_train, epochs=7, conv_activation='linear', model_name='cnn.mdl')
--- hw1/mypackages/predict_best.py
+++ hw1/mypackages/predict_best.py
@@ -1,7 +1,7 @@
 import sys
 from utils import *
 
-load_timit_dataset(sys.argv[1])
+load_timit_dataset(dataset_path=sys.argv[1])
 _, _, X_test, id_test, label_binarizer = preprocess()
 predict(model_name='best.mdl', X_test=X_test, id_test=id_test, 
 		label_binarizer=label_binarizer, threshold=0.65, output_file=sys.argv[2])
