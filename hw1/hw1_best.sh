31440a67ffd49a0e0878cabcde83730b264f1136
Yu-Jing Lin <linx1045@umn.edu>
2017-10-28 18:36:12 +0800

    revised again

--- hw1/hw1_best.sh
+++ hw1/hw1_best.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -O models/bset.mdl "https://www.dropbox.com/s/g082zogqk15rye3/best.mdl?dl=1"
+wget -O models/best.mdl "https://www.dropbox.com/s/g082zogqk15rye3/best.mdl?dl=1"
 python mypackages/predict_best.py "$1" "$2"
--- hw1/mypackages/predict_rnn.py
+++ hw1/mypackages/predict_rnn.py
@@ -5,3 +5,4 @@ load_timit_dataset(dataset_path=sys.argv[1])
 _, _, X_test, id_test, label_binarizer = preprocess()
 predict(model_name='rnn.mdl', X_test=X_test, id_test=id_test, 
 		label_binarizer=label_binarizer, threshold=0.7, output_file=sys.argv[2])
+print('# Prediction finished.')
