1c1c97e780371a6bc42c309bae21ef16e1cbfcb1
Elvis Yu-Jing Lin <linx1045@umn.edu>
2017-11-12 16:04:32 +0800

    moved the model for special mission upto Dropbox

--- hw2/hw2_special.sh
+++ hw2/hw2_special.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+wget -O "models/hw2_special.ckpt.data-00000-of-00001" "https://www.dropbox.com/s/6j9oy40cs8dlcj7/hw2_special.ckpt.data-00000-of-00001?dl=1"
 python hw2_special.py "$1" "$2"
