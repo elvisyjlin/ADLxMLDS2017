0756651e16689dd74d9abb871e8125f51a434de4
Elvis Yu-Jing Lin <linx1045@umn.edu>
2017-11-19 23:20:03 +0800

    revised

--- hw2/hw2_seq2seq.sh
+++ hw2/hw2_seq2seq.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# TODO!!!!!
-wget -O "[model name]" "[model url]"
-python hw2_seq2seq.py "$1" "[model]" "$2"
-python hw2_seq2seq.py "$1/peer_review" "[model]"  "$3"
+wget -O "models/hw2_as2vt.ckpt.data-00000-of-00001" "https://www.dropbox.com/s/1c5bp6khol8li04/hw2_as2vt.data-00000-of-00001?dl=1"
+python hw2_seq2seq.py "$1" "hw2_as2vt" "$2"
+python hw2_seq2seq.py "$1/peer_review" "hw2_as2vt"  "$3"
